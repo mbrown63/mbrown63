a95c2dcb4b12dc5238cd83a94cc42dfcbfd2a488
Mi'Kayla Brown <mikaylabrown@MiKaylas-MacBook-Pro.local>
2026-09-24 23:57:57 -0400

    desktop layout is done

---
+++ csce242/exercises/04/script/slideshow.js
@@ -0,0 +1,6 @@
+//when the right arrow is clicked 
+document.getElementById("hero-arrow-right").onclick = (e) => {
+    e.preventDefault():
+    const currentSlide = document.querySelectorAll("#slides :not(.hidden")[0]
+    console.log(currentSlide);
+};
